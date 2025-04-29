2f8134e57b2025287a9d60bfcf101299e52c8dc8
VLSMB <vlsmb@foxmail.com>
2025-04-29 15:10:30 +0800

    reset

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include "loginwindow.h"
-#include<iostream>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     LoginWindow w;
-    std::cout<<"nnnnnnnnnn"<<std::endl;
     w.show();
     return a.exec();
 }
